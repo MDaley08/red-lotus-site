679688aca4b9fa58911a1c4aa5f3c791905b465d
Michael Daley <mgd3@njit.edu>
2024-06-07 22:27:54 -0400

    completed styling of background, adjusted dropdown menu for smaller screens

--- js/nav.js
+++ js/nav.js
@@ -1,31 +1,47 @@
-const sidebarManager = (() => {
-    const sidebar = document.querySelector('.sidebar');
-    const sidebarBtn = document.getElementById('sidebar-button');
+const menuManager = (() => {
+    const dropdownMenu = document.querySelector('.dropdown')
+    const dropdownBtn = document.getElementById('menu-button');
     const closeBtn = document.getElementById('close-button');
-    const smallWindowSize = 950;
-    sidebarBtn.addEventListener('click', () => {
-        sidebar.style.display = 'flex'
+    const smallWindowSize = 800;
+    let menuOpen = false;
 
+    dropdownBtn.addEventListener('click', () => {
+        dropdownMenu.style.display = 'flex';
+        dropdownBtn.style.display = 'none';
+        closeBtn.style.display = 'block';
+        menuOpen = true;
     });
 
     closeBtn.addEventListener('click', () => {
-        sidebar.style.display = 'none'
+        closeBtn.style.display = 'none';
+        dropdownBtn.style.display = 'block';
+        dropdownMenu.style.display = 'none';
+        menuOpen = false;
     });
 
     window.addEventListener("resize", () => {
         if(window.innerWidth > smallWindowSize){
-            sidebar.style.display = 'none';
+            dropdownBtn.style.display = 'none';
+            dropdownMenu.style.display = 'none';
+            menuOpen = false;
         }
-        
+        else if(!menuOpen){
+            dropdownBtn.style.display = 'block';
+        }
     });
+
+    window.addEventListener("load", () => {
+        if(window.innerWidth <= smallWindowSize) dropdownBtn.style.display = 'block'
+    });
+
 })();
 
-const joinButton = ((link) => {
-    const button = document.getElementById('join-button');
-    const openDiscord = () => {
-        window.open(link, 'test')
-    }
-    button.addEventListener('click', openDiscord);
-})('test.com');
+// const joinButton = ((link) => {
+//     const button = document.getElementById('join-button');
+//     const openDiscord = () => {
+//         window.open(link, 'test')
+//     }
+//     button.addEventListener('click', openDiscord);
+// })('test.com');
 
 
