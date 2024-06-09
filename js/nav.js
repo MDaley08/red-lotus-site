b52b9787c0813a2b7ed849e244e758254dd0d4e3
Michael Daley <mgd3@njit.edu>
2024-06-09 17:03:25 -0400

    refactored nav bar styling and code

--- js/nav.js
+++ js/nav.js
@@ -1,37 +1,10 @@
 const menuManager = (() => {
-    const dropdownMenu = document.querySelector('.dropdown')
-    const dropdownBtn = document.getElementById('menu-button');
-    const closeBtn = document.getElementById('close-button');
-    const smallWindowSize = 800;
-    let menuOpen = false;
+    const navMenu = document.querySelector('.dropdown');
+    const hamburgerDiv = document.querySelector('.hamburger');
 
-    dropdownBtn.addEventListener('click', () => {
-        dropdownMenu.style.display = 'flex';
-        dropdownBtn.style.display = 'none';
-        closeBtn.style.display = 'block';
-        menuOpen = true;
-    });
-
-    closeBtn.addEventListener('click', () => {
-        closeBtn.style.display = 'none';
-        dropdownBtn.style.display = 'block';
-        dropdownMenu.style.display = 'none';
-        menuOpen = false;
-    });
-
-    window.addEventListener("resize", () => {
-        if(window.innerWidth > smallWindowSize){
-            dropdownBtn.style.display = 'none';
-            dropdownMenu.style.display = 'none';
-            menuOpen = false;
-        }
-        else if(!menuOpen){
-            dropdownBtn.style.display = 'block';
-        }
-    });
-
-    window.addEventListener("load", () => {
-        if(window.innerWidth <= smallWindowSize) dropdownBtn.style.display = 'block'
+    hamburgerDiv.addEventListener('click', () => {
+        hamburgerDiv.classList.toggle('hamburger-open');
+        navMenu.classList.toggle('dropdown-open');
     });
 
 })();
